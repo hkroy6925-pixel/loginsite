984b0261bc0e0396c163ea0b82181a6dd26b3b49
hkroy6925-pixel <hkroy6925@gmail.com>
2026-03-27 17:07:09 +0900

    Update index.js

--- index.js
+++ index.js
@@ -66,6 +66,8 @@ app.post("/register", async (req, res) => {
 });
 
 // --------------------
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
-  console.log(`서버 실행: http://localhost:${PORT}`);
+  console.log(`서버 실행: ${PORT}`);
 });
